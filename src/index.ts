3011ff33568dc310811d9ddff22b31013f359125
Mark kibocha <mark.varu@student.cuk.ac.ke>
2024-08-04 21:24:36 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const app = express(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(compression());
 app.use(cookieParser());
 app.use("/api/v1", routes)
